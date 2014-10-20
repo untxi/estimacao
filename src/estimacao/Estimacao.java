8dd3e04b2031d48c94f70b69d3d8b0280dc7b8a3
untxi <samarburola@gmail.com>
2014-10-19 19:30:00 -0600

    Prototipo: Barra Menú Lista (en letras)

--- src/estimacao/Estimacao.java
+++ src/estimacao/Estimacao.java
@@ -5,6 +5,8 @@
  */
 package estimacao;
 
+import GUI.ventanaPrincipal;
+
 /**
  *
  * @author Samantha
@@ -16,6 +18,8 @@ public class Estimacao {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola, soy estimacao");
+        
     }
     
 }
