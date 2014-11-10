c65637a8da5333280f4904cdf6902830ba699632
marsep27 <marsep27@hotmail.com>
2014-11-09 20:51:14 -0600

    reportar mascota, Botón reportar

--- src/GUI/VentanaReportarMascota.java
+++ src/GUI/VentanaReportarMascota.java
@@ -6,6 +6,7 @@
 package GUI;
 
 import Controladores.ControladorReportes;
+import Estructuras.stringNumerico;
 import Interfaces.IConstantes;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
@@ -260,13 +261,16 @@ public class VentanaReportarMascota extends javax.swing.JInternalFrame implement
     }//GEN-LAST:event_fieldUbicacionActionPerformed
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+        if(stringNumerico.isNumeric(fieldIdChip.getText())){
         ControladorReportes.getInstance().reportarMascota(fieldNombreMascota.getText(),
         boxRaza.getSelectedItem().toString(), boxEspecie.getSelectedItem().toString(), boxTamaño1.getSelectedItem().toString(),
         fieldPelaje.getText(), fieldOjos.getText(),fieldUbicacion.getText(), boxTamaño.getSelectedItem().toString(),
         fieldNotas.getText(), " ", Integer.parseInt(fieldIdChip.getText()), false, buscarFecha.getCalendar());
-        
         JOptionPane.showMessageDialog(null, "Reporte realizado con éxito");
         btnCancelarActionPerformed(evt);
+        }else{
+        JOptionPane.showMessageDialog(null, "El espacio para el chip debe tener 00 si la mascota no tiene chip"); 
+        }
     }//GEN-LAST:event_btnBuscarActionPerformed
 
     private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
